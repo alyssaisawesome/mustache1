9b4ba8f9240b9c3b807c5ade0e1b9d50799f97ff
alyssaisawesome <86136406+alyssaisawesome@users.noreply.github.com>
2022-03-10 22:04:58 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload() {
+
+}
+
+function setup() {
+  canvas=createCanvas(350,300);
+  canvas.center();
+  video= createCapture(VIDEO);
+  video.size(350,300);
+  video.hide();
+}
+
+
+
+function draw() {
+  image(video, 0, 0, 350, 300);
+}
+
+function snapshot() {
+save('myfilterimg.png');
+}
